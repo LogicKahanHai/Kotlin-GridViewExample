1fb689e04a6352483b5c72f4bc83ec9280dffb39
Logic - Rishi Bhalla <contact.logickahanhai@gmail.com>
2024-08-14 13:34:14 +0530

    Grid View Languages example complete.

--- app/src/main/java/com/example/customlistview/CustomGridLayoutExample.kt
+++ app/src/main/java/com/example/customlistview/CustomGridLayoutExample.kt
@@ -1,7 +1,9 @@
 package com.example.customlistview
 
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.GridView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,18 +13,29 @@ class CustomGridLayoutExample : AppCompatActivity() {
     lateinit var courseGV: GridView
     lateinit var courseList: List<GridViewModel>
 
-    /*
-            Images:-
-            1. C++
-            2. Kotlin
-            3. Java
-            4. Android Studio
-     */
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_custom_grid_layout_example)
+
+        courseGV = findViewById(R.id.GRV)
+        courseList = ArrayList<GridViewModel>()
+
+        courseList = courseList + GridViewModel("C++", R.drawable.cplusplus)
+        courseList = courseList + GridViewModel("Java", R.drawable.java)
+        courseList = courseList + GridViewModel("Kotlin", R.drawable.kotlin)
+        courseList = courseList + GridViewModel("Python", R.drawable.python)
+        courseList = courseList + GridViewModel("Android Studio", R.drawable.androidstudio)
+        courseList = courseList + GridViewModel("React Native", R.drawable.ic_launcher_background)
+        courseList = courseList + GridViewModel("Flutter", R.drawable.ic_launcher_foreground)
+
+        val courseAdapter = GridViewAdapter(courseList, this)
+        courseGV.adapter = courseAdapter
+
+        courseGV.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            Toast.makeText(applicationContext, courseList[position].courseName + " selected", Toast.LENGTH_SHORT).show()
+        }
+
 
     }
 }
